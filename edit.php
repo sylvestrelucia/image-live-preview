5817de5fe905f36154c27073742f7c07874091af
Sylvestre Lucia <sylvestre.lucia@me.com>
2017-06-09 11:41:33 +0200

    Add npm setup

---
+++ edit.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["url"]; ?><br>
+Your email address is: <?php echo $_POST["login"]; ?>
+Your email address is: <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="edit.php" method="post">
+    URL: <input type="text" name="url"><br>
+    login: <input type="text" name="login"><br>
+    password: <input type="password" name="password"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
