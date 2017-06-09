34c44839b593525ba14a6781b20ee44791e7ff44
Sylvestre Lucia <sylvestre.lucia@me.com>
2017-06-09 15:35:52 +0200

    Add kvasir

--- edit.php
+++ edit.php
@@ -1,9 +1,14 @@
+<?php require 'lib/copy.php'; ?>
+
 <html>
 <body>
 
+<div class=m-nav>
 Welcome <?php echo $_POST["url"]; ?><br>
 Your email address is: <?php echo $_POST["login"]; ?>
 Your email address is: <?php echo $_POST["password"]; ?>
+</div>
+
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<html>
-<body>
+<?php include "inc/header.inc" ?>
 
-<form action="edit.php" method="post">
-    URL: <input type="text" name="url"><br>
-    login: <input type="text" name="login"><br>
-    password: <input type="password" name="password"><br>
-    <input type="submit">
-</form>
 
-</body>
-</html>
+<div class="kvasir">
+    <div class="t-index">
+        <div class="o-form o-form--full" action="edit.php" method="post">
+            <div class="o-form__center">
+                <div class="o-form__title">Live responsive image testing</div>
+                <p class="o-form__desc">Paste here URL you want to edit</p>
+                    <input class="o-form__form--main" type="text" name="url"><br>
+                    <p class="o-form__optional">Optional</p>
+                    <span class="o-form__asterix">login</span><input class="o-form__form" type="text" name="login"><br>
+                    <span class="o-form__asterix">password</span><input class="o-form__form--main"  type="password" name="password"><br>
+                    <input class="a-button a-button--filled-red" type="submit" value="Start live editing">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<?php include "inc/footer.inc" ?>
