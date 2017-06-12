40d779382ef87226724827f3b44defd354aee7d6
Sylvestre Lucia <sylvestre.lucia@me.com>
2017-06-12 18:33:42 +0200

    Add proof of concept image preview

--- edit.php
+++ edit.php
@@ -15,6 +15,10 @@
 </head>
 <body>
 
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+<script src="http://<?php echo $server_url ?>/js/edit.js"></script>
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" async=""></script>
 
 <div class="kvasir m-nav">
@@ -28,8 +32,43 @@
     }
 ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">" async=""></script>
-<script src="http://<?php echo $server_url ?>/js/edit.js"></script>
+<div id="wrapper"><input class="js-fileUpload" multiple="multiple" type="file"/>
+    <div class="js-image-holder"></div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $(".js-fileUpload").on('change', function() {
+            //Get count of selected files
+            var countFiles = $(this)[0].files.length;
+            var imgPath = $(this)[0].value;
+            var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+            var image_holder = $(".js-image-holder");
+            image_holder.empty();
+            if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+                if (typeof(FileReader) != "undefined") {
+                    //loop for each file selected for uploaded.
+                    for (var i = 0; i < countFiles; i++)
+                    {
+                        var reader = new FileReader();
+                        reader.onload = function(e) {
+                            $("<img />", {
+                                "src": e.target.result,
+                                "class": "thumb-image"
+                            }).appendTo(image_holder);
+                        }
+                        image_holder.show();
+                        reader.readAsDataURL($(this)[0].files[i]);
+                    }
+                } else {
+                    alert("This browser does not support FileReader.");
+                }
+            } else {
+                alert("Pls select only images");
+            }
+        });
+    });
+</script>
 
 </body>
 </html>
--- js/edit.js
+++ js/edit.js
@@ -2,14 +2,13 @@ $(document).ready(function(){
 
     const editor =
         "<div class='js-edit__wrapper'>" +
-        "<span class='js-edit__upload js-edit__upload--mobile'><i class='fa fa-mobile'></i></span>" +
+        "<span class='js-edit__upload js-edit__upload--mobile'><i class='fa fa-mobile'></i>" +
+        "<div id='wrapper'><input class='js-fileUpload' multiple='multiple' type='file'/>" +
+        "<div class='js-image-holder'></div>" +
+        "</div>" +
+        "</span>" +
         "<span class='js-edit__upload js-edit__upload--desktop'><i class='fa fa-desktop'></i></span>" +
         "</div>";
-
-    function change_image(image) {
-
-
-    }
 
     function wrap_images() {
         $("img").wrap("<div class='js-edit__editor'></div>");
@@ -28,15 +27,9 @@ $(document).ready(function(){
         });
 
 
-
-
         $("img").mouseout(function(){
             $(this).find(".js-edit__wrapper").remove();
         });
-    }
-    
-    function upload_image() {
-        
     }
 
 
